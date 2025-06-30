5a14ef56fb0b1b9acd46964f4f29988d2f04475e
Muhammad Fauzan Anwar <zans.anwar@gmail.com>
2025-06-30 08:06:36 +0700

    update

--- src/js/tiketTersedia.js
+++ src/js/tiketTersedia.js
@@ -2,13 +2,23 @@ const dataTiket = [
   ["Argo Bromo", "Surabaya", "06:00", 150000],
   ["Taksaka", "Yogyakarta", "07:30", 200000],
   ["Lodaya", "Bandung", "08:15", 170000],
-  ["Parahyangan", "Jakarta", "05:45", 180000],
+  ["Argo Parahyangan", "Jakarta", "05:45", 180000],
   ["Serayu", "Purwokerto", "10:00", 130000],
   ["Matarmaja", "Malang", "11:20", 90000],
   ["Gajayana", "Blitar", "09:00", 160000],
   ["Sancaka", "Yogyakarta", "12:10", 140000],
   ["Singasari", "Pasuruan", "13:30", 120000],
   ["Majapahit", "Semarang", "14:45", 110000],
+  ["Argo Wilis", "Surabaya", "15:00", 190000],
+  ["Turangga", "Solo", "16:20", 150000],
+  ["Jayakarta", "Jakarta", "17:15", 135000],
+  ["Kutojaya", "Kutoarjo", "18:00", 100000],
+  ["Tawang Jaya", "Tegal", "19:40", 115000],
+  ["Kaligung", "Brebes", "20:10", 95000],
+  ["Bogowonto", "Kebumen", "21:30", 105000],
+  ["Progo", "Wates", "22:00", 98000],
+  ["Menoreh", "Magelang", "22:45", 110000],
+  ["Senja Utama", "Cirebon", "23:15", 125000],
 ];
 
 const container = document.getElementById("ticketContainer");
@@ -26,6 +36,7 @@ dataTiket.forEach(([kereta, tujuan, jam, harga], index) => {
       </div>
       <span class="text-xs bg-white text-indigo-600 font-semibold px-2 py-1 rounded-full">Ticket</span>
     </div>
+    
     <div class="space-y-2 text-sm">
       <div class="flex justify-between">
         <span class="text-gray-500">Tujuan</span>
